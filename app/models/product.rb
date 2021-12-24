6599026076c05dce5d186ae407163b3a0c430be3
German Gallardo <ggallardoh.90@gmail.com>
2021-12-24 13:03:20 -0300

    pushg

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,8 @@ class Product < ApplicationRecord
   after_create_commit { broadcast_prepend_to 'products' }
   after_update_commit { broadcast_replace_to 'products' }
   after_destroy_commit { broadcast_remove_to 'products' }
+
+  validates :name, uniqueness: true, presence: true
+  validates :stock, numericality: { only_integer: true }, presence: true
+  validates :description, presence: true
 end
